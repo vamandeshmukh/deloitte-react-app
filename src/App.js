dcfc4a121561a3aecdd0be676a07c2b535ceaf68
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-16 11:25:14 +0530

    react-basics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
 
-function App() {
+// // class component 
+
+
+// import React from "react";
+
+// class App extends React.Component {
+
+//   render = () => {
+//     return (
+
+//       <div>
+//         <p >Welcome</p>
+//         <p>Welcome to Deloitte React App</p>
+//       </div>
+
+//     );
+//   };
+// }
+
+// export default App;
+
+// function components 
+// component - modern JS arrow function 
+
+import Home from "./components/Home";
+
+const App = () => {
 
   return (
-    <div>
-      <p>Welcome</p>
+    <div >
+      <p >Welcome</p>
       <p>Welcome to Deloitte React App</p>
+      <Home />
     </div>
   );
-}
 
+}
 export default App;
+
+// component - old JS function 
+// function App() {
+//   return (
+//     <div>
+//       <p >Welcome</p>
+//       <p>Welcome to Deloitte React App</p>
+//     </div>
+//   );
+// }
+// export default App;
+
+
+
+
+
+
+// Web apps 
+// SPA - Single page apps 
+// MPA - Multi page apps 
+// Responsive web app 
+// Mobile-first approach 
+
+
+
+// components 
+// a functionality that makes a complete page or a part of page 
+
+// React - types of components -
+// 1. class component
+// 2. function component - prefered 
+
 
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+
+const Home = () => {
+
+    return (
+        <div >
+            <p >Welcome</p>
+            <p>Welcome to Home component</p>
+            <Home />
+        </div>
+    );
+
+}
+export default Home;
