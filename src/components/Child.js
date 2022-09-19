3c03e9e3e7a8adb9415f9e6e18e9f33504186f3e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-19 14:43:18 +0530

    react-basics

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,16 +1,46 @@
+import { useState } from "react";
 
 const Child = (props) => {
 
+    const [childData, setChildData] = useState(20);
     const parentDataInChild = props.dataFromParent;
+
+    const sendDataToParent = () => {
+        console.log(`Child function invoked.`)
+        props.passData(childData);
+    }
 
     return (
         <div >
             <p>Child component</p>
-            <p>Parent data in child component:  
-                {parentDataInChild} 
-                {props.anotherProperty}
+            <p>Chld data in child component:
+                {childData}
             </p>
+            <p>Parent data in child component:
+                {parentDataInChild}
+            </p>
+            <input type="button" value="Pass data" onClick={sendDataToParent} />
+
         </div>
     );
 }
 export default Child;
+
+
+
+
+// const Child = (props) => {
+
+//     const parentDataInChild = props.dataFromParent;
+
+//     return (
+//         <div >
+//             <p>Child component</p>
+//             <p>Parent data in child component:
+//                 {parentDataInChild}
+//                 {props.anotherProperty}
+//             </p>
+//         </div>
+//     );
+// }
+// export default Child;
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -4,16 +4,51 @@ import Child from './Child';
 const Parent = () => {
 
     const [parentData, setParentData] = useState(10);
+    const [childDataInParent, setChildDataInParent] = useState(0);
+
+    const receiveDataFromChild = (arg) => {
+        console.log(`Parent function invoked.`)
+        setChildDataInParent(arg);
+    }
 
     return (
         <div >
             <p>Parent component</p>
             <p>Parent data in parent component: {parentData}</p>
+            <p>Child data in parent component: {childDataInParent} </p>
             <Child
                 dataFromParent={parentData}
-                anotherProperty='Abcd' />
+                passData={receiveDataFromChild}
+            />
         </div>
     );
 
 }
 export default Parent;
+
+
+
+
+
+// import { useState } from 'react';
+// import Child from './Child';
+
+// const Parent = () => {
+
+//     const [parentData, setParentData] = useState(10);
+
+//     return (
+//         <div >
+//             <p>Parent component</p>
+//             <p>Parent data in parent component: {parentData}</p>
+//             <Child
+//                 dataFromParent={parentData}
+//                 anotherProperty='Abcd' />
+//         </div>
+//     );
+
+// }
+// export default Parent;
+
+
+
