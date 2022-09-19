95f1c2490b2beba95c0ba512540bb17f528e1ddd
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-19 11:57:45 +0530

    react-basics

--- src/components/EmpData.js
+++ src/components/EmpData.js
@@ -16,8 +16,11 @@ const EmpData = () => {
             <p>EmpData Component</p>
             <div>
                 <input type="number" name="eid" value={emp.eid} onChange={handleEmp} />
+                <br />
                 <input type="text" name="firstName" value={emp.firstName} onChange={handleEmp} />
+                <br />
                 <input type="number" name="salary" value={emp.salary} onChange={handleEmp} />
+                <br />
                 {/* <input type="text" name="eid" value={} onChange={} /> */}
 
             </div>
