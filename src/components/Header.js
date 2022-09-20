171a96dd973908eedae2babd2b8ef6f31a0bf0cd
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-20 15:28:24 +0530

    react-basics

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,45 @@ const Header = () => {
 
     return (
         <div>
-            <Link to='/emp'>Emp</Link>
-            <Link to='/parent'>Parent</Link>
-            <Link to='/login'>Login</Link>
-            <Link to='/home'>Home</Link>
+            <div>
+                <Link to='/emp'>Emp</Link>
+                <Link to='/parent'>Parent</Link>
+                <Link to='/login'>Login</Link>
+                <Link to='/home'>Home</Link>
+            </div>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <Link className="navbar-brand" to="#">Navbar</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <Link className="nav-link" to="#">Home <span className="sr-only">(current)</span></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="#">Link</Link>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle" to="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </Link>
+                                <div className="dropdown-menu">
+                                    <Link className="dropdown-item" to="#">Action</Link>
+                                    <Link className="dropdown-item" to="#">Another action</Link>
+                                    <div className="dropdown-divider"></div>
+                                    <Link className="dropdown-item" to="#">Something else here</Link>
+                                </div>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link disabled">Disabled</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         </div>
     );
 
