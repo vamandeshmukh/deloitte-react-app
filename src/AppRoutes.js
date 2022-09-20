a5f98ec369870c8f9cac6c097e4168a666047be4
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-20 14:56:50 +0530

    react-basics

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EmpData from './components/EmpData';
+import Header from "./components/Header";
 import Home from './components/Home';
 import Login from './components/Login';
 import Parent from './components/Parent';
@@ -8,19 +9,21 @@ const AppRoutes = () => {
 
     return (
         <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/emp" element={<EmpData />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/parent" element={<Parent />} />
-                    <Route path="/" element={<Home />} />
-
-                    {/* <Route path="/login" > <Login /> </Route> */}
-
-                </Routes>
-            </BrowserRouter>
-
+            <div>
+                <Header />
+            </div>
+            <div>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/emp" element={<EmpData />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/parent" element={<Parent />} />
+                        <Route path="/" element={<Home />} />
+                        {/* <Route path="/login" > <Login /> </Route> */}
+                    </Routes>
+                </BrowserRouter>
+            </div>
         </div>
     );
 };
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+const Header = () => {
+
+    return (
+        <div>
+            <p>Header Component</p>
+        </div>
+    );
+
+}
+export default Header;
