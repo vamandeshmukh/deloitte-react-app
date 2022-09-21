ab95e9b29b3bc6a484d55b89b82d1901bc776760
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-21 15:44:49 +0530

    axios

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,3 +1,5 @@
+
+
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -5,9 +7,9 @@ const BlogPost = () => {
 
     const url = `http://localhost:12345`;
 
+    const [postId, setPostId] = useState('');
     const [todaysPost, setTodaysPost] = useState({});
     const [postToPublish, setPostToPublish] = useState({});
-    const [postId, setPostId] = useState('');
 
     useEffect(
         () => {
@@ -17,9 +19,8 @@ const BlogPost = () => {
                 title: '',
                 body: ''
             });
-            setPostToPublich({
+            setPostToPublish({
                 userId: '',
-                id: '',
                 title: '',
                 body: ''
             });
@@ -33,18 +34,25 @@ const BlogPost = () => {
         setPostId(evt.target.value);
     };
 
+    const handlePostToPublishInput = (evt) => {
+        setPostToPublish({
+            ...postToPublish,
+            [evt.target.name]: evt.target.value
+        });
+    };
 
     const getBlogPostData = (evt) => {
 
         // axios.get().then().catch();
         axios.get(`${url}/posts/${postId}`)
             .then((response) => {
-                console.log(response);
                 console.log(response.data);
                 setTodaysPost(response.data);
+                setPostId(``);
             }).catch((error) => {
                 console.log(error.message);
                 alert(`Post with postId ${postId} not found!`);
+                setPostId(``);
             });
         evt.preventDefault();
     }
@@ -52,14 +60,10 @@ const BlogPost = () => {
     const sendBlogPostData = (evt) => {
         console.log(`send`);
 
-        axios.post(`${url}/posts/`, {
-            userId: 8,
-            id: ``,
-            title: `Happy Weekend`,
-            body: `Weekends are a fantastic set of days...`
-        })
+        axios.post(`${url}/posts/`, postToPublish)
             .then((response) => {
                 console.log(response.data);
+                alert(`Your blog post with postId ${response.data.id} has been created successfully!`);
             })
             .catch((error) => {
                 console.log(error.message);
@@ -74,23 +78,73 @@ const BlogPost = () => {
         <div className="container">
             <p className="display-4 text-primary ">My Blog Post</p>
             <hr />
-            <input type="number" value={postId} className="form-control col-4" onChange={handlePostIdInput} placeholder="Please enter postId" />
-            <input type="button" value="View Post" className="btn btn-outline-primary mt-3 mb-3" onClick={getBlogPostData} />
-            <div>
-                <p>Title: {todaysPost.title}</p>
-                <p>userId: {todaysPost.userId}</p>
-                <p>id: {todaysPost.id}</p>
-                <p>body: {todaysPost.body}</p>
-            </div>
+            <div className="row justify-content-center">
+                <div className="col-5 mx-3 my-3 py-3 bg-white shadow">
+                    <p className="lead">Search a Blog Post by Id</p>
+                    <form class="form-inline">
+                        <input
+                            type="number"
+                            name="postId"
+                            value={postId}
+                            onChange={handlePostIdInput}
+                            className="form-control mr-2"
+                            placeholder="Please enter postId"
+                        />
+                        <input
+                            type="submit"
+                            value="Search Post"
+                            className="btn btn-outline-primary form-control"
+                            onClick={getBlogPostData} />
+                    </form>
+                    <hr />
+                    <div>
+                        <p>Title: {todaysPost.title}</p>
+                        <p>userId: {todaysPost.userId}</p>
+                        <p>id: {todaysPost.id}</p>
+                        <p>body: {todaysPost.body}</p>
+                    </div>
+                </div>
 
-            <div>
-                {/* // here  */}
-                <input type="button" value="Create Post" className="btn btn-outline-primary mt-3 mb-3" onClick={sendBlogPostData} />
+                <div className="col-5 mx-3 my-3 py-3 bg-white shadow">
+                    <p className="lead">Create a New Blog Post</p>
+                    <form>
+                        <input
+                            type="number"
+                            name="userId"
+                            value={postToPublish.userId}
+                            className="form-control mt-3 mb-3"
+                            placeholder="Please enter userId"
+                            onChange={handlePostToPublishInput}
+                        />
+                        <input
+                            type="text"
+                            name="title"
+                            value={postToPublish.title}
+                            className="form-control mt-3 mb-3"
+                            placeholder="Please enter title"
+                            onChange={handlePostToPublishInput}
+                        />
+                        <input
+                            type="textarea"
+                            name="body"
+                            value={postToPublish.body}
+                            className="form-control mt-3 mb-3"
+                            placeholder="Please enter body"
+                            onChange={handlePostToPublishInput}
+                        />
+                        <input
+                            type="submit"
+                            value="Create Post"
+                            className="btn btn-outline-primary mt-3 mb-3"
+                            onClick={sendBlogPostData}
+                        />
+                    </form>
+                </div>
             </div>
         </div>
     );
-
 };
 
 export default BlogPost;
+
 
