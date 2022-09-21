aef9ead8160d55c803550b7cb4c29903d0327ace
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-21 10:54:13 +0530

    axios

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -4,8 +4,7 @@ import { useEffect, useState } from "react";
 const BlogPost = () => {
 
     const [todaysPost, setTodaysPost] = useState({});
-
-    // https://jsonplaceholder.typicode.com/posts/2
+    const [postId, setPostId] = useState('');
 
     useEffect(
         () => {
@@ -20,9 +19,15 @@ const BlogPost = () => {
         []
     );
 
+    const handlePostIdInput = (evt) => {
+        console.log(postId);
+        setPostId(evt.target.value);
+    };
+
+
     const getBlogPostData = (evt) => {
         console.log(`getBlogPostData`);
-        axios.get(`https://jsonplaceholder.typicode.com/posts/2`)
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
             .then((response) => {
                 console.log(response.data);
                 setTodaysPost(response.data);
@@ -34,7 +39,8 @@ const BlogPost = () => {
         <div className="container">
             <p className="display-4 text-primary ">My Blog Post</p>
             <hr />
-            <input type="button" value="View Post" className="btn btn-outline-primary mb-3" onClick={getBlogPostData} />
+            <input type="number" value={postId} className="form-control col-4" onChange={handlePostIdInput} placeholder="Please enter postId" />
+            <input type="button" value="View Post" className="btn btn-outline-primary mt-3 mb-3" onClick={getBlogPostData} />
             <div>
                 <p>Title: {todaysPost.title}</p>
                 <p>userId: {todaysPost.userId}</p>
